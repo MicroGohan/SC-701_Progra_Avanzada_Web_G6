9205569dc255246f3ac8ba04fcc46f8b1b05862f
Wilson G <106784185+MicroGohan@users.noreply.github.com>
2025-06-10 17:00:21 -0600

    create project
    Create base project

---
+++ WD/Weather_G6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Weather_G6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
