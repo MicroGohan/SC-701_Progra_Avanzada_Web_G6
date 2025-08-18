31643583ff0d2d6c6f6ad68c5c2e4f6eb24da42c
Diego-Valladares <diego14vb@gmail.com>
2025-08-18 16:18:01 -0600

    Correccion de error
    Se me fue un "/" extra jaja

--- WD/WD.Mvc/Models/UserSettingsViewModel.cs
+++ WD/WD.Mvc/Models/UserSettingsViewModel.cs
@@ -5,9 +5,9 @@ public class UserSettingsViewModel
 {
     // Perfil
     public string Nombre { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty; /
+    public string Email { get; set; } = string.Empty;
     public string? Continente { get; set; }
-    public string UnidadTemperatura { get; set; } = "C"; 
+    public string UnidadTemperatura { get; set; } = "C";
     public bool TopFavoritosPublico { get; set; }
     public string? PasswordActual { get; set; }
     public string? PasswordNuevo { get; set; }
